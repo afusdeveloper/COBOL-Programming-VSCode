05d27ad51f581debabc824f1692f8036543049cf
[afusdeveloper] <[aymara.fusaro@gmail.com]>
2021-11-15 19:45:55 -0300

    ...

--- CBL/CBL0004.cbl
+++ CBL/CBL0004.cbl
@@ -21,82 +21,83 @@
       ******************************************************************
       *
        01  PRINT-REC.
-           05  ACCT-NO-O      PIC X(8).
-           05  FILLER         PIC X(02) VALUE SPACES.
+           05  ACCT-NO-O               PIC X(8).
+           05  FILLER                  PIC X(02) VALUE SPACES.
       ******************************************************************
       *    FILLER -- COBOL reserved word used as data name to remove   *
       *    the need of variable names only for inserting spaces        *
       ******************************************************************
       *
-           05  LAST-NAME-O    PIC X(20).
-           05  FILLER         PIC X(02) VALUE SPACES.
+           05  LAST-NAME-O             PIC X(20).
+           05  FILLER                  PIC X(02) VALUE SPACES.
       ******************************************************************
       *    SPACES -- used for structured spacing data outputs rather   *
       *    than using a higher PIC Clause length as in CBL0001.cobol,  *
       *    which makes a good design practice and a legible output     *
       ******************************************************************
       *
-           05  ACCT-LIMIT-O   PIC $$,$$$,$$9.99.
+           05  ACCT-LIMIT-O            PIC $$,$$$,$$9.99.
       ******************************************************************
       *    The repeated $ characters revert to spaces and then one $   *
       *    in front of the printed amount.                             *
       ******************************************************************
       *
-           05  FILLER         PIC X(02) VALUE SPACES.
-           05  ACCT-BALANCE-O PIC $$,$$$,$$9.99.
-           05  FILLER         PIC X(02) VALUE SPACES.
+           05  FILLER                  PIC X(02) VALUE SPACES.
+           05  ACCT-BALANCE-O          PIC $$,$$$,$$9.99.
+           05  FILLER                  PIC X(02) VALUE SPACES.
        FD  ACCT-REC RECORDING MODE F.
        01  ACCT-FIELDS.
-           05  ACCT-NO            PIC X(8).
-           05  ACCT-LIMIT         PIC S9(7)V99 COMP-3.
-           05  ACCT-BALANCE       PIC S9(7)V99 COMP-3.
-           05  LAST-NAME          PIC X(20).
-           05  FIRST-NAME         PIC X(15).
+           05  ACCT-NO                 PIC X(8).
+           05  ACCT-LIMIT              PIC S9(7)V99 COMP-3.
+           05  ACCT-BALANCE            PIC S9(7)V99 COMP-3.
+           05  LAST-NAME               PIC X(20).
+           05  FIRST-NAME              PIC X(15).
            05  CLIENT-ADDR.
-               10  STREET-ADDR    PIC X(25).
-               10  CITY-COUNTY    PIC X(20).
-               10  USA-STATE      PIC X(15).
-           05  RESERVED           PIC X(7).
-           05  COMMENTS           PIC X(50).
+               10  STREET-ADDR         PIC X(25).
+               10  CITY-COUNTY         PIC X(20).
+               10  USA-STATE           PIC X(15).
+           05  RESERVED                PIC X(7).
+           05  COMMENTS                PIC X(50).
       *
        WORKING-STORAGE SECTION.
        01 FLAGS.
-           05 LASTREC         PIC X VALUE SPACE.
+           05 LASTREC                  PIC X VALUE SPACE.
       *
        01  HEADER-1.
-           05  FILLER         PIC X(20) VALUE 'Financial Report for'.
-           05  FILLER         PIC X(60) VALUE SPACES.
+           05  FILLER                  PIC X(20) VALUE 
+                                       'Financial Report for'.
+           05  FILLER                  PIC X(60) VALUE SPACES.
       *
        01  HEADER-2.
-           05  FILLER         PIC X(05) VALUE 'Year '.
-           05  HDR-YR         PIC 9(04).
-           05  FILLER         PIC X(02) VALUE SPACES.
-           05  FILLER         PIC X(06) VALUE 'Month '.
-           05  HDR-MO         PIC X(02).
-           05  FILLER         PIC X(02) VALUE SPACES.
-           05  FILLER         PIC X(04) VALUE 'Day '.
-           05  HDR-DAY        PIC X(02).
-           05  FILLER         PIC X(56) VALUE SPACES.
+           05  FILLER                  PIC X(05) VALUE 'Year '.
+           05  HDR-YR                  PIC 9(04).
+           05  FILLER                  PIC X(02) VALUE SPACES.
+           05  FILLER                  PIC X(06) VALUE 'Month '.
+           05  HDR-MO                  PIC X(02).
+           05  FILLER                  PIC X(02) VALUE SPACES.
+           05  FILLER                  PIC X(04) VALUE 'Day '.
+           05  HDR-DAY                 PIC X(02).
+           05  FILLER                  PIC X(56) VALUE SPACES.
       *
        01  HEADER-3.
-           05  FILLER         PIC X(08) VALUE 'Account '.
-           05  FILLER         PIC X(02) VALUE SPACES.
-           05  FILLER         PIC X(10) VALUE 'Last Name '.
-           05  FILLER         PIC X(15) VALUE SPACES.
-           05  FILLER         PIC X(06) VALUE 'Limit '.
-           05  FILLER         PIC X(06) VALUE SPACES.
-           05  FILLER         PIC X(08) VALUE 'Balance '.
-           05  FILLER         PIC X(40) VALUE SPACES.
+           05  FILLER                  PIC X(08) VALUE 'Account '.
+           05  FILLER                  PIC X(02) VALUE SPACES.
+           05  FILLER                  PIC X(10) VALUE 'Last Name '.
+           05  FILLER                  PIC X(15) VALUE SPACES.
+           05  FILLER                  PIC X(06) VALUE 'Limit '.
+           05  FILLER                  PIC X(06) VALUE SPACES.
+           05  FILLER                  PIC X(08) VALUE 'Balance '.
+           05  FILLER                  PIC X(40) VALUE SPACES.
       *
        01  HEADER-4.
-           05  FILLER         PIC X(08) VALUE '--------'.
-           05  FILLER         PIC X(02) VALUE SPACES.
-           05  FILLER         PIC X(10) VALUE '----------'.
-           05  FILLER         PIC X(15) VALUE SPACES.
-           05  FILLER         PIC X(10) VALUE '----------'.
-           05  FILLER         PIC X(02) VALUE SPACES.
-           05  FILLER         PIC X(13) VALUE '-------------'.
-           05  FILLER         PIC X(40) VALUE SPACES.
+           05  FILLER                  PIC X(08) VALUE '--------'.
+           05  FILLER                  PIC X(02) VALUE SPACES.
+           05  FILLER                  PIC X(10) VALUE '----------'.
+           05  FILLER                  PIC X(15) VALUE SPACES.
+           05  FILLER                  PIC X(10) VALUE '----------'.
+           05  FILLER                  PIC X(02) VALUE SPACES.
+           05  FILLER                  PIC X(13) VALUE '-------------'.
+           05  FILLER                  PIC X(40) VALUE SPACES.
       *
       ******************************************************************
       *  HEADER -- structures for report or column headers,            *
@@ -127,17 +128,17 @@
       ******************************************************************
       *
        WRITE-HEADERS.
-           MOVE FUNCTION CURRENT-DATE TO WS-CURRENT-DATE-DATA.
-           MOVE WS-CURRENT-YEAR  TO HDR-YR.
-           MOVE WS-CURRENT-MONTH TO HDR-MO.
-           MOVE WS-CURRENT-DAY   TO HDR-DAY.
-           WRITE PRINT-REC FROM HEADER-1.
-           WRITE PRINT-REC FROM HEADER-2.
-           MOVE SPACES TO PRINT-REC.
-           WRITE PRINT-REC AFTER ADVANCING 1 LINES.
-           WRITE PRINT-REC FROM HEADER-3.
-           WRITE PRINT-REC FROM HEADER-4.
-           MOVE SPACES TO PRINT-REC.
+           MOVE FUNCTION CURRENT-DATE  TO WS-CURRENT-DATE-DATA.
+           MOVE WS-CURRENT-YEAR        TO HDR-YR.
+           MOVE WS-CURRENT-MONTH       TO HDR-MO.
+           MOVE WS-CURRENT-DAY         TO HDR-DAY.
+           WRITE PRINT-REC             FROM HEADER-1.
+           WRITE PRINT-REC             FROM HEADER-2.
+           MOVE SPACES                 TO PRINT-REC.
+           WRITE PRINT-REC             AFTER ADVANCING 1 LINES.
+           WRITE PRINT-REC             FROM HEADER-3.
+           WRITE PRINT-REC             FROM HEADER-4.
+           MOVE SPACES                 TO PRINT-REC.
       *
        READ-NEXT-RECORD.
            PERFORM READ-RECORD
@@ -164,13 +165,13 @@
       *
        READ-RECORD.
            READ ACCT-REC
-           AT END MOVE 'Y' TO LASTREC
+           AT END MOVE 'Y'             TO LASTREC
            END-READ.
       *
        WRITE-RECORD.
-           MOVE ACCT-NO      TO  ACCT-NO-O.
-           MOVE ACCT-LIMIT   TO  ACCT-LIMIT-O.
-           MOVE ACCT-BALANCE TO  ACCT-BALANCE-O.
-           MOVE LAST-NAME    TO  LAST-NAME-O.
+           MOVE ACCT-NO                TO  ACCT-NO-O.
+           MOVE ACCT-LIMIT             TO  ACCT-LIMIT-O.
+           MOVE ACCT-BALANCE           TO  ACCT-BALANCE-O.
+           MOVE LAST-NAME              TO  LAST-NAME-O.
            WRITE PRINT-REC.
       *
